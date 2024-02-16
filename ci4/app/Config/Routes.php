ab5b7451e802604ee466fe16fc9a3e2a6549adf0
Timothy Jay <tvsayson@student.apc.edu.ph>
2024-02-16 21:13:58 +0800

    awit

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -3,6 +3,11 @@
 use CodeIgniter\Router\RouteCollection;
 
 /**
- * @var RouteCollection $routes
- */
+ 
+@var RouteCollection $routes*/
 $routes->get('/', 'Home::index');
+
+use App\Controllers\Pages;
+
+$routes->get('pages', [Pages::class, 'index']);
+$routes->get('(:segment)', [Pages::class, 'view']);
---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,1 @@
+Welcome to CI4 Webprog MI221!
