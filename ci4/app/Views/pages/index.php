8d137d98003968c10a7c499c76513d85fa3415a6
Timothy Jay <tvsayson@student.apc.edu.ph>
2024-02-16 22:42:58 +0800

    Test 3

--- ci4/app/Views/pages/index.php
+++ ci4/app/Views/pages/index.php
@@ -223,7 +223,7 @@ function test_input($data) {
   Gender:
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
+  <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Others  
   <span class="error">* <?php echo $genderErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
